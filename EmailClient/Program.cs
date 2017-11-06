91789337d9aba80cd6d03e297b350d9fb812db7c
Marko Milojevic <milojevicmarko92@gmail.com>
2017-11-07 00:54:44 +0100

    Add console application.

---
+++ EmailClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using EmailValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var email = "user@test.com";
+            var validator = new EmailValidator();
+
+            var result = validator.IsValid(email) ? "Valid" : "Invalid";
+
+            Console.WriteLine($"{result} - {email}");
+            Console.ReadKey();
+        }
+    }
+}
