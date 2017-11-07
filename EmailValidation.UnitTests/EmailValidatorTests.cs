892e650a05491b1b5edae84aac38e4afb21d17fd
Marko Milojevic <milojevicmarko92@gmail.com>
2017-11-07 21:58:06 +0100

    Add validation for blacklisted emails.

--- EmailValidation.UnitTests/EmailValidatorTests.cs
+++ EmailValidation.UnitTests/EmailValidatorTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using Xunit;
 
 namespace EmailValidation.UnitTests
 {
@@ -21,6 +22,20 @@ namespace EmailValidation.UnitTests
         public void Should_ReturnFalse_When_EmailIsInvalid(string email)
         {
             Assert.False(new EmailValidator().IsValid(email));
+        }
+
+        [Theory]
+        [InlineData("test@gmail.com", new[] { "user@gmail.com", "test@microsoft.com", "foo@outlook.com" })]
+        public void Should_ReturnTrue_When_EmailIsNotBlacklisted(string email, string[] blacklistedEmails)
+        {
+            Assert.True(new EmailValidator(blacklistedEmails).IsValid(email));
+        }
+
+        [Theory]
+        [InlineData("test@gmail.com", new[] { "user@gmail.com", "test@gmail.com", "foo@outlook.com" })]
+        public void Should_ReturnFalse_When_EmailIsBlacklisted(string email, string[] blacklistedEmails)
+        {
+            Assert.False(new EmailValidator(blacklistedEmails).IsValid(email));
         }
     }
 }
--- EmailValidation/EmailValidator.cs
+++ EmailValidation/EmailValidator.cs
@@ -1,17 +1,50 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace EmailValidation
 {
     public class EmailValidator
     {
-        public bool IsValid(string email)
+        private readonly IEnumerable<string> _blacklistedEmails = Enumerable.Empty<string>();
+        private readonly bool _checkIsEmailBlacklisted = false;
+
+        public EmailValidator()
         {
-            return IsFormatValid(email);
         }
 
-        private bool IsFormatValid(string email)
+        public EmailValidator(IEnumerable<string> blacklistedEmails)
         {
-            return new EmailAddressAttribute().IsValid(email);
+            _blacklistedEmails = blacklistedEmails ?? throw new ArgumentNullException(nameof(blacklistedEmails));
+            _checkIsEmailBlacklisted = true;
+        }
+
+        public bool IsValid(string email)
+        {
+            if (IsNotValidFormat(email))
+            {
+                return false;
+            }
+
+            if (_checkIsEmailBlacklisted && IsEmailBlacklisted(email))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsNotValidFormat(string email)
+        {
+            var isValid = new EmailAddressAttribute().IsValid(email);
+
+            return !isValid;
+        }
+
+        private bool IsEmailBlacklisted(string email)
+        {
+            return _blacklistedEmails.Any(blacklistedEmail => blacklistedEmail == email);
         }
     }
 }
